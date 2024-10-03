3901cabe77bee10239f3f7ecc18f8285cf370e0f
NathanGRSDesktop <nathangeers00@gmail.com>
2024-10-03 19:52:35 +0200

    3-10 changes final?

--- Main.php
+++ Main.php
@@ -8,6 +8,7 @@
 <body>
     <div id="MainContent">
         <?php
+        // require_once used to access the class file.
         require_once 'member.php';
         session_start();
 
@@ -16,19 +17,21 @@
             $_SESSION["users"] = array();
         }
 
+        // Deletes members / Destroys the session.
         if (isset($_POST["MembersDel"])){
             if($_POST["MembersDel"]){
-                session_destroy();
-                echo "De leden zijn succesvol verwijderd. <br/>";
-                echo "<button class='Button' onclick=location.href='index.html'>Terug</button>";
+                $memberDelete = new memberController();
+                echo $memberDelete->deleteMember();
                 exit();
             }
         }
+
         // Shows the amount of member(s) if the button is pressed for it.
         if (isset($_POST["MembersCount"])){
             if($_POST["MembersCount"]){
                 $memberCount = new memberController();
                 echo $memberCount->countMembers();
+                // echo'ing the button here, not in member.php because of repeat code.
                 echo "<button class='Button' onclick=location.href='index.html'>Terug</button>";
                 exit();
             }
@@ -62,11 +65,8 @@
 
             // Iterates through members and calls the getMember() function in member.php.
             if (isset($_POST["MembersSum"])){
-                if ($_POST["MembersSum"]){
-                    if (count($_SESSION["users"]) == 0){
-                        echo "Er zijn geen leden momenteel.";
-                    }
-                    // Loops through the users list, looping through the user_data, looping through each key and value and printing the values.
+                if ($_POST["MembersSum"])
+                    // Loops through the users and prints the info. No more double foreach loops needed.
                     echo "<div id='FormInfo'>";
                         foreach ($_SESSION["users"] as $member){
                             echo "<div id='PersonalInfo'>";
@@ -77,9 +77,8 @@
                     echo "</div>";
                     echo "<button class='Button' onclick=location.href='index.html'>Terug</button>";
                     exit();
-                }
             }
-        // Flavor text as confirmation :)
+        // Flavor text as confirmation, only runs when making new members, the reason for the usage of exit()'s :)
         echo "De registratie is gelukt! Wij zien u graag snel bij Fit4You!<br/>";
         echo "<button class='Button' onclick=location.href='index.html'>Terug</button>";
         ?>
